228a652162671bd2aab1f1186331ac68de0d3dc6
Carson Holloway <carsonalh@gmail.com>
2024-12-06 12:06:28 +1000

    Development/production mode based of running flag

--- src/hiveserver/main.go
+++ src/hiveserver/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"errors"
+	"flag"
 	"github.com/joho/godotenv"
 	"log"
 	"net/http"
@@ -23,15 +24,30 @@ type serverState struct {
 
 var state *serverState
 
-func withHeaders(next http.Handler) http.Handler {
+func withHeaders(next http.Handler, restrictOrigin bool) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
-		w.Header().Set("Access-Control-Allow-Origin", "*")
+		if restrictOrigin {
+			w.Header().Set("Access-Control-Allow-Origin", "https://hivegame.io")
+		} else {
+			w.Header().Set("Access-Control-Allow-Origin", "*")
+		}
 		next.ServeHTTP(w, r)
 	})
 }
 
 func createServer() *http.Server {
+	var useTls bool
+	var restrictOrigin bool
+
+	var development bool
+
+	flag.BoolVar(&development, "development", false, "Set to run in development mode")
+	flag.Parse()
+
+	useTls = !development
+	restrictOrigin = !development
+
 	state = new(serverState)
 	state.pendingGameCondition = sync.NewCond(&sync.Mutex{})
 
@@ -43,17 +59,27 @@ func createServer() *http.Server {
 
 	mux := http.NewServeMux()
 
-	mux.Handle("GET /join", new(joinHandler))
-	mux.Handle("GET /play", NewPlayHandler())
+	mux.Handle("GET /join", withHeaders(new(joinHandler), restrictOrigin))
+	mux.Handle("GET /play", withHeaders(NewPlayHandler(), restrictOrigin))
+
+	mux.Handle("/", http.FileServer(http.Dir("./static/")))
 
 	server := &http.Server{
 		Addr:    ":" + port,
-		Handler: withHeaders(mux),
+		Handler: mux,
 	}
 
 	go func() {
-		if err := server.ListenAndServe(); !errors.Is(err, http.ErrServerClosed) {
-			log.Fatal(err)
+		if useTls {
+			log.Println("Server starting (HTTPS)")
+			if err := server.ListenAndServeTLS("master.crt", "hivegame.pem"); !errors.Is(err, http.ErrServerClosed) {
+				log.Fatal(err)
+			}
+		} else {
+			log.Println("Server starting (HTTP)")
+			if err := server.ListenAndServe(); !errors.Is(err, http.ErrServerClosed) {
+				log.Fatal(err)
+			}
 		}
 	}()
 
